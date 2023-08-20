e8e83319a9f3e731d7c4cdac30e4a74b5b2ca0f4
Theodore <theo.daizer@gmail.com>
2023-08-20 17:48:15 +0500

    Window creation realization

--- executor.py
+++ executor.py
@@ -1,1 +1,15 @@
-print('Hello Git!')
+import sys
+import pygame
+
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+
+
+if __name__ == '__main__':
+    pygame.init()
+    windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
